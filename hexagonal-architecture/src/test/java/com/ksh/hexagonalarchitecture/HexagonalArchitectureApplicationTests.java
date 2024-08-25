75113b5ea8a4b660d70fc7b7f45db0e4efdc5a53
HanSeoJin <yp6484@gmail.com>
2024-08-25 11:07:22 +0900

    new project

---
+++ hexagonal-architecture/src/test/java/com/ksh/hexagonalarchitecture/HexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksh.hexagonalarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
